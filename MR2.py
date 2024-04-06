98234b93e2ec0461ccb0626d08fce9a35465c234
Sathvik <Sathvik.Rao@iiitb.ac.in>
2024-04-06 18:02:30 +0530

    changes to mr2

--- MR2.py
+++ MR2.py
@@ -1,19 +1,37 @@
-from sympy import Symbol
-from sympy.stats import DiscreteUniform,sample
+# Description: This file contains the implementation of the Miller-Rabin primality test.
+from sympy.stats import DiscreteUniform
 from arithmeticOps import *
+import time,random
 class Primality:
     def __init__(self,x,T):
         self.x=x
         self.T=T
-    def _checkInWn(self,a):
-        if(Modn(power(a,self.x-1),self.x)==1):
-            pass
+        self.h,self.t=removeFactorOf2(x-1)
+       
+
+    def _checkInWn(self,a): # Witness set test
+        if(powerModN(a,self.x-1,self.x)==1):
+            b=powerModN(a,self.t,self.x)
+            if b==1:
+                return True
+            for i in range(0,self.h):
+                if(b-self.x==-1):
+                    return True
+                if(b==1):
+                    return False
+                b=Modn(b*b,self.x)
+            return False
+        return False
+
 
     def checkPrimality(self):
-        X=DiscreteUniform("X",range(1,self.x))
-        random_int=X.pspace.sample()
-        return random_int[X]
+        
+        for i in range(0,self.T):
+            random_int=random.randint(1,self.x-1)
+            if not self._checkInWn(random_int):
+                return False
+        return True
 
 
-
+print(Primality(89,100).checkPrimality())
 
--- arithmeticOps.py
+++ arithmeticOps.py
@@ -1,3 +1,4 @@
+
 def Modn(x,n): # x mod n
     store=[n] # contains 2^i*n
     while(True):
@@ -13,16 +14,35 @@ def Modn(x,n): # x mod n
         i=i-1
     return x
     
-    
+def removeFactorOf2(n): # n=2^h.t
+    h=0
+    while(Modn(n,2)==0):
+        h=h+1
+        n=n>>1
+    return (h,n)
+
 def power(x,n): # x^n
-    a=sympy.Integer(x)
+    a=x
     bin_string=[]
     while(n!=0):
-        bin_string.append(Modn(x,2))
+        bin_string.append(Modn(n,2))
         n=n>>1
     c=1
-    for i in range(len(bin_string)-1,-1,-1):
+    for i in range(0,len(bin_string)):
         if(bin_string[i]):
             c*=a
         a=a*a
     return c
+def powerModN(x,n,m): # x^n mod m
+    a=x
+    bin_string=[]
+    while(n!=0):
+        bin_string.append(Modn(n,2))
+        n=n>>1
+    c=1
+    for i in range(len(bin_string)):
+        if(bin_string[i]):
+            c=Modn(c*a,m)
+        a=Modn(a*a,m)
+    return c
+# print(powerModN(91113,286540,286541))
