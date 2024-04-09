7cdda19032860dadfe1936e8afed57fc074a3877
Shreyas529 <Shreyas.Saggere@iiitb.ac.in>
2024-04-09 13:05:51 +0530

    Added thue lemma functionality

--- BEGCD.py
+++ BEGCD.py
@@ -1,7 +1,15 @@
 from arithmeticOps import Modn , power
 from gmpy2 import mpz
 
+opt_r = []
+opt_s = []
+opt_t = []
+ 
 def begcd(a,b):
+    global opt_r,opt_s,opt_t
+    opt_r.clear()
+    opt_s.clear()
+    opt_t.clear()
     e = 0
     
     while Modn(a,2)==0 and Modn(b,2)==0:
@@ -39,6 +47,9 @@ def begcd(a,b):
         s2-=s1
         t2-=t1
         
+        opt_r.append(a*power(2,e))
+        opt_s.append(s1)
+        opt_t.append(t1)
         # print(s1*a2+t1*b2==r1)
         # print(s2*a2+t2*b2==r2)
         
@@ -50,10 +61,32 @@ def begcd(a,b):
     
     return [a * power(2,e),s1,t1]
         
+def thue(n,b,r,t):
+    global opt_r,opt_s,opt_t
+    result = begcd(n,b)
+    j=-1
+    
+    if r > n or n > r*t:
+        raise Exception("Incorrect values of n , b , r* , t*")
+    
+    for i in range(len(opt_r)):
+        if opt_r[i] < r:
+            j=i
+            break
+    
+    if j!=-1:
+        return [opt_r[j],opt_t[j]]
+    else:
+        raise Exception("Couldn't find rj < r*") 
         
-a , b = 9999999999999999999999999999999999999999988888878 , 1234567890098765432112345123456789009876543211234416
-# a , b = 23 , 120
-gcd1 , s , t = begcd(int(a),int(b))
-gcd2 = s*a + t*b
-print(gcd1 , s , t)
-print(gcd2)
+# a , b = 9999999999999999999999999999999999999999988888878 , 1234567890098765432112345123456789009876543211234416
+# # a , b = 23 , 120
+# gcd1 , s , t = begcd(int(a),int(b))
+# gcd2 = s*a + t*b
+# print(gcd1 , s , t)
+# print(gcd2)
+
+n , b, r, t = 7, 31, 4, 12
+result = thue(n,b,r,t)
+print(result)
+assert result[0]%n==(b*result[1])%n
