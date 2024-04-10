1acd0ab8b51d9d6d9a62efd11feb321b44b385dd
Sathvik <Sathvik.Rao@iiitb.ac.in>
2024-04-10 15:09:14 +0530

    finished thue and added division operation in arithmethicOps

--- BEGCD.py
+++ BEGCD.py
@@ -11,11 +11,15 @@ def begcd(a,b):
     opt_s.clear()
     opt_t.clear()
     e = 0
-    
+    r=0
+    t=0
+   
     while Modn(a,2)==0 and Modn(b,2)==0:
         e+=1
         a = a>>1
         b = b>>1
+       
+        
     
     a2 , b2 = a , b
     s1, s2 = 1, 0
@@ -29,13 +33,13 @@ def begcd(a,b):
             
             s1 >>=1
             t1 >>=1
+            
             assert s1*a2+t1*b2==a
         
         while Modn(b,2)==0:
             b=b>>1
             if(Modn(s2,2)==1 or Modn(t2,2)==1):
                 s2 , t2 = s2+b2 , t2-a2
-            
             s2 >>=1
             t2 >>=1
             assert s2*a2+t2*b2==b
@@ -49,33 +53,15 @@ def begcd(a,b):
         s2-=s1
         t2-=t1
         
+        
         opt_r.append(a*power(2,e))
         opt_s.append(s1)
         opt_t.append(t1)
     
+        
     return [a * power(2,e),s1,t1]
         
-def thue(n,b,r,t):
-    global opt_r,opt_s,opt_t
-    result = begcd(n,b)
-    j=-1
-    
-    # if r > n or n > r*t:
-    #     raise Exception("Incorrect values of n , b , r* , t*")
-    
-    for i in range(len(opt_r)):
-        if opt_r[i] < r:
-            j=i
-            break
-    
-    if j!=-1:
-        return [opt_r[j],opt_t[j]]
-    else:
-        raise Exception("Couldn't find rj < r*") 
+
         
 if __name__=="__main__":
-    a , b = 9999999999999999999999999999999999999999988888878 , 1234567890098765432112345123456789009876543211234416
-    gcd1 , s , t = begcd(int(a),int(b))
-    gcd2 = s*a + t*b
-    print(gcd1 , s , t)
-    print(gcd2)
+    print(begcd(47247096489788,22160927059))
